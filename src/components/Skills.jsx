e0b0aa016aae44be78506a3c666f9b88b71454d5
Darshan Raj <darshankollam8@gmail.com>
2023-11-11 13:34:45 +0530

    skills ui set up

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,42 +1,104 @@
 import React from "react";
-import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs, FaDatabase } from 'react-icons/fa';
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaNodeJs,
+  FaDatabase,
+  FaArrowRight,
+} from "react-icons/fa";
+import {BsArrowRightCircleFill, BsGit} from "react-icons/bs"
+import {DiMongodb} from "react-icons/di"
+import {SiPostman, SiSocketdotio} from "react-icons/si"
+import {SiTailwindcss} from "react-icons/si"
+import {IoLogoFigma} from "react-icons/io5"
+
+
 
 export default function Skills() {
   return (
+    <section className="py-10 flex flex-col items-center">
+    <h2 className="text-2xl font-bold mb-6">What I Am Good At?</h2>
 
-    <section className="py-10">
-    <h2 className="text-2xl font-bold mb-4">Skills</h2>
-    <ul className="list-disc ml-8">
-      <li>
-        <FaHtml5 className="inline-block mr-2" />
-        HTML
+    <ul className="list-disc ml-0 mb-8 flex flex-wrap justify-center">
+      {/* Display the first 6 items in a row */}
+      <li className="flex items-center mb-4 flex-shrink-0 mr-6">
+        <div className="rounded-full bg-black p-3">
+          <FaHtml5 className="text-white text-4xl md:text-5xl lg:text-6xl" />
+        </div>
       </li>
-      <li>
-        <FaCss3 className="inline-block mr-2" />
-        CSS
+      <li className="flex items-center mb-4 flex-shrink-0 mr-6">
+        <div className="rounded-full bg-black p-3">
+          <FaCss3 className="text-white text-4xl md:text-5xl lg:text-6xl" />
+        </div>
       </li>
-      <li>
-        <FaJs className="inline-block mr-2" />
-        JavaScript
+      <li className="flex items-center mb-4 flex-shrink-0 mr-6">
+        <div className="rounded-full bg-black p-3">
+          <FaJs className="text-white text-4xl md:text-5xl lg:text-6xl" />
+        </div>
       </li>
-      <li>
-        <FaReact className="inline-block mr-2" />
-        React Js
+      <li className="flex items-center mb-4 flex-shrink-0 mr-6">
+        <div className="rounded-full bg-black p-3">
+          <FaReact className="text-white text-4xl md:text-5xl lg:text-6xl" />
+        </div>
       </li>
-      <li>
-        <FaNodeJs className="inline-block mr-2" />
-        Node Js
+      <li className="flex items-center mb-4 flex-shrink-0 mr-6">
+        <div className="rounded-full bg-black p-3">
+          <FaNodeJs className="text-white text-4xl md:text-5xl lg:text-6xl" />
+        </div>
       </li>
-      <li>
-        <FaNodeJs className="inline-block mr-2" />
-        Express Js
+      <li className="flex items-center mb-4 flex-shrink-0 mr-6">
+        <div className="rounded-full bg-black p-3">
+          <BsGit className="text-white text-4xl md:text-5xl lg:text-6xl" />
+        </div>
       </li>
-      <li>
-        <FaDatabase className="inline-block mr-2" />
-        MongoDB
+
+      {/* Center the remaining items below */}
+      <li className="flex items-center mb-4 flex-shrink-0 mr-6">
+        <div className="rounded-full bg-black p-3">
+          <DiMongodb className="text-white text-4xl md:text-5xl lg:text-6xl" />
+        </div>
       </li>
+      <li className="flex items-center mb-4 flex-shrink-0 mr-6">
+        <div className="rounded-full bg-black p-3">
+          <SiSocketdotio className="text-white text-4xl md:text-5xl lg:text-6xl" />
+        </div>
+      </li>
+      <li className="flex items-center mb-4 flex-shrink-0 mr-6">
+        <div className="rounded-full bg-black p-3">
+          <SiTailwindcss className="text-white text-4xl md:text-5xl lg:text-6xl" />
+        </div>
+      </li>
+      <li className="flex items-center mb-4 flex-shrink-0 mr-6">
+        <div className="rounded-full bg-black p-3">
+          <IoLogoFigma className="text-white text-4xl md:text-5xl lg:text-6xl" />
+        </div>
+      </li>
+      <li className="flex items-center mb-4 flex-shrink-0 mr-6">
+        <div className="rounded-full bg-black p-3">
+          <SiPostman className="text-white text-4xl md:text-5xl lg:text-6xl" />
+        </div>
+      </li>
+      {/* Add more items as needed */}
     </ul>
+
+    {/* Points and Arrow Icons */}
+    <div className="flex flex-col items-start mb-4 text-left">
+      <div className="flex items-center mb-2">
+        <BsArrowRightCircleFill className="text-2xl text-black mt-1 mr-2" />
+        <p className="text-lg md:text-xl lg:text-2xl font-medium">
+          Building Restful API in Express
+        </p>
+      </div>
+      <div className="flex items-center">
+        <BsArrowRightCircleFill className="text-2xl text-black mt-1 mr-2" />
+        <p className="text-lg md:text-xl lg:text-2xl font-medium">
+          Building Responsive Single Page Application
+        </p>
+      </div>
+    </div>
   </section>
-   
+
   );
 }
